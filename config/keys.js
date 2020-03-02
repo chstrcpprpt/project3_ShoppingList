7ef414ca6b25a6f5525c84bab039d2be25b44792
chstrcpprpt <cjhallen@gmail.com>
2020-03-02 18:20:33 +1100

    initial server setup

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const password = encodeURIComponent("I87cookies!");
+
+module.exports = {
+  mongoURI: `mongodb+srv://chstrcpprpt:${password}@shoppingcartdb-yyxsd.mongodb.net/test?retryWrites=true&w=majority`
+};
