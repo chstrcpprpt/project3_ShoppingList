a46d4244ffb8ae0828107d896c3c65227eb3f678
chstrcpprpt <cjhallen@gmail.com>
2020-03-06 21:19:23 +1100

    added db GET, POST & DELETE routes

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,5 +4,32 @@ const router = express.Router();
 // Item model
 const Item = require("../../models/Item");
 
+// GET items
+router.get('/', (req, res) => {
+  Item.find()
+    .sort({date: -1})
+    .then(items => res.json(items));
+});
+
+// POST - create items
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name,
+    section: req.body.section
+  });
+
+  newItem.save()
+    .then(item => res.json(item));
+});
+
+// DELETE - remove items
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove()
+      .then(() => res.json({success: true}))
+    )
+    .catch(err => res.status(404).json({success: false}));
+});
+
 
 module.exports = router;
