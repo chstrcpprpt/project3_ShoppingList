983058fad85fb49495376bec7362ae27a0feb43e
chstrcpprpt <cjhallen@gmail.com>
2020-03-07 12:07:29 +1100

    Added Navbar component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import NavbarApp from "./components/Navbar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <NavbarApp />
     </div>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,48 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from "reactstrap";
+
+class NavbarApp extends Component {
+  state = {
+    isOpen: false
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">Shopping List</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/chstrcpprpt">Github</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+    
+  }
 
 }
 
-export default Navbar;
+export default NavbarApp;
