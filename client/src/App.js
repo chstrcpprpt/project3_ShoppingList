d7bd83a2cb1831f7510e014510542afd6ba62b1a
chstrcpprpt <cjhallen@gmail.com>
2020-03-21 10:36:29 +1100

    Wrapped with provider

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 import NavbarApp from "./components/Navbar";
 import ShoppingList from "./components/ShoppingList";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <NavbarApp />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <NavbarApp />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 
