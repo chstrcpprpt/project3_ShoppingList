0ed163827fd5f8525cad8da4973a2cf3e1533590
chstrcpprpt <cjhallen@gmail.com>
2020-03-21 11:13:50 +1100

    Added modal to add items

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import NavbarApp from "./components/Navbar";
 import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -13,7 +15,10 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <NavbarApp />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
